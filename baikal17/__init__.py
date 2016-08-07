f950f5bfd967c3932e074314bf0f196cceb590d8
Vadim Iskuchekov <vadim.deus@gmail.com>
2016-08-07 18:36:00 +1000

    Add version. Change license in __init__.py

---
+++ baikal17/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    baikal17
+    ~~~~~~~~
+
+    Baikal-17 is a simple text quest engine.
+
+    :copyright: (c) 2016 by Vadim Iskuchekov
+    :license: MIT, see LICENSE for more details.
+"""
+
+__version__ = '0.1-dev'
